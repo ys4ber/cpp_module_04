d6b94a7e812e308bd360d42db4b094a9802eab39
ys4ber <youssefsaber1337@gmail.com>
2024-08-21 16:15:39 +0100

    adding logical main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -18,11 +18,12 @@ int main()
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
-    
-    
     ICharacter* bob = new Character("bob");
     me->use(0, *bob);
     me->use(1, *bob);
+    me->unequip(2);
+    me->use(2, *bob);
+    me->equip(tmp);
     me->use(2, *bob);
     me->use(3, *bob);
     delete bob;
