13a11f447c428eaf592f11f3807c8a4698db2165
ys4ber <youssefsaber1337@gmail.com>
2024-08-21 08:46:01 +0100

    adding a strong main function

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -8,21 +8,41 @@
 int main()
 {
     IMateriaSource* src = new MateriaSource();
-    src->learnMateria(new Ice());
-    src->learnMateria(new Cure());
+    Ice ice;
     ICharacter* me = new Character("me");
     AMateria* tmp;
+    src->learnMateria(&ice);
+    src->learnMateria(new Cure());
     tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    me->equip(tmp);
+    ICharacter* youssef = new Character("youssef");
+    ICharacter* hamid = new Character("hamid");
+    hamid->equip(tmp);
+    hamid->equip(tmp);
+    hamid->equip(tmp);
+    hamid->equip(tmp);
+    delete hamid;
+    youssef->equip(tmp);
+    youssef->equip(tmp);
+    youssef->equip(tmp);
+    tmp = src->createMateria("cure");
     me->equip(tmp);
     tmp = src->createMateria("cure");
     me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
+    me->unequip(2);
     ICharacter* bob = new Character("bob");
     me->use(0, *bob);
     me->use(1, *bob);
-    
+    me->use(2, *bob);
+    me->use(3, *bob);
+    delete youssef;
     delete bob;
     delete me;
     delete src;
-
     return 0;
 }
