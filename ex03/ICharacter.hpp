8e3f1df8ba072f91098f925eeb4a6f3fc4b3ac9b
ys4ber <youssefsaber1337@gmail.com>
2024-08-21 08:52:31 +0100

    modifying the interfaces

--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -9,10 +9,7 @@ class ICharacter
 {
 
     public:
-        ICharacter();
-        ICharacter(ICharacter const & other);
-        ICharacter & operator=(ICharacter const & other);
-        virtual ~ICharacter();
+        virtual ~ICharacter() {}
         virtual std::string const & getName() const = 0;
         virtual void equip(AMateria* m) = 0;
         virtual void unequip(int idx) = 0;
